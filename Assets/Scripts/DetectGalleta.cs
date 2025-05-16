475b53411760b2dc90a1d521ef5c0371e716f2c5
a01752433 <149410194+a01752433@users.noreply.github.com>
2025-05-15 22:23:47 -0600

    sistena de cuerdas

--- Assets/Scripts/DetectGalleta.cs
+++ Assets/Scripts/DetectGalleta.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
+
 public class DetectGalleta : MonoBehaviour
 {
     private string mensaje = "Comido";
+
+    void Start()
+    {
+        // Esto ya no es necesario, trabajaremos directamente con el objeto de colisión
+    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Galleta"))
         {
             Debug.Log(mensaje);
-            Destroy(collision.gameObject, 2);
+            Destroy(collision.gameObject);  // Destruye el objeto que colisionó
         }
     }
 }
-
