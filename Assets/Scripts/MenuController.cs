de6e27cdac6918fee9d71beae3579a34fcd8e6b0
a01752433 <149410194+a01752433@users.noreply.github.com>
2025-05-17 00:06:49 -0600

    pantallaInicioCreditos

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -6,17 +6,40 @@ using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour
 {
     private Button btnJugar;
+    private Button btnCreditos;
+    private UIDocument uIDocumentCreditos;
+    private VisualElement rootCreditos;
 
     void OnEnable()
     {
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+
         btnJugar = root.Q<Button>("PlayButton");
+        btnCreditos = root.Q<Button>("CreditButton");
+
+        GameObject uiCreditosGO = GameObject.Find("UICreditos");
+        uIDocumentCreditos = uiCreditosGO.GetComponent<UIDocument>();
+        rootCreditos = uIDocumentCreditos.rootVisualElement;
+
         btnJugar.clicked += Juego;
+        btnCreditos.clicked += Creditos;
 
     }
-    
-    // wwww
-    private void Juego() {
+
+    private void Juego()
+    {
         SceneManager.LoadScene("Nivel1");
+    }
+
+    private void Creditos()
+    {
+    VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+    root.visible = false;
+    rootCreditos.visible = true;
+
+    // Mostrar el contenedor principal de créditos
+    var contenedorPopUp = rootCreditos.Q<VisualElement>("ContenedorPrincipal");
+    if (contenedorPopUp != null)
+        contenedorPopUp.visible = true;
     }
 }
